f44f5b23f3eb3bd718553c4e9cb1c30e05a21c48
DESKTOP-IOGDLC4\Bucu <jonatan.reky@gmail.com>
2025-06-13 19:13:49 +0700

    Ready to Prod

--- vite.config.ts
+++ vite.config.ts
@@ -12,16 +12,15 @@
 //     },
 //   },
 // })
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
-  base: '/mantabjo/', // <== HARUS sesuai dengan nama repository GitHub
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
-    },
-  },
+      '@': path.resolve(__dirname, 'src')
+    }
+  }
 })
