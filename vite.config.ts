a6bafc3f8d7edbd375cf2927d6bec787fc637a2c
DESKTOP-IOGDLC4\Bucu <jonatan.reky@gmail.com>
2025-06-13 18:32:38 +0700

    Deploy Aplikasi

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import path from 'path'
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   base: process.env.NODE_ENV === 'production' ? '/MantabJo/' : '/',
+//   resolve: {
+//     alias: {
+//       '@': path.resolve(__dirname, './src'),
+//     },
+//   },
+// })
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
+  base: '/mantabjo/', // Ganti dengan nama repo GitHub-mu
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/MantabJo/' : '/',
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-})
+});
