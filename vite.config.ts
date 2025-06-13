e855045e19717524faf4371c14923edff95ba70e
DESKTOP-IOGDLC4\Bucu <jonatan.reky@gmail.com>
2025-06-13 08:15:03 +0700

    first deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import path from 'path'
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   base: process.env.NODE_ENV === 'production' ? '/MantabJo/' : '/',
+//   resolve: {
+//     alias: {
+//       '@': path.resolve(__dirname, './src'),
+//     },
+//   },
+// })
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// Ganti dengan nama repo kamu
+const repoName = 'mantabjo';
+
 export default defineConfig({
+  base: `/${repoName}/`,
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/MantabJo/' : '/',
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-})
+});
